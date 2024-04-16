5d31dffa4f88632bb3c3c91d423c56f6fa2cf1e2
DampAdam <57593619+DampAdam@users.noreply.github.com>
2024-04-16 02:48:42 +0200

    Update api.py

--- api.py
+++ api.py
@@ -2,10 +2,9 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/api', methods=['POST'])
+@app.route('/api', methods=['GET'])
 def api():
-    data = request.get_json()
-    return jsonify(data)
+    return jsonify({'message': 'Hello, World!'})
 
 if __name__ == '__main__':
     app.run(debug=True)
